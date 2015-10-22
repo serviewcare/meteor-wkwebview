44e194fea90177e1e6410d2ae30f03693ea676e6
Jason Thomas <jthomas@silvervue.com>
2015-10-22 13:58:56 -0600

    Actually Fix typo

--- package.js
+++ package.js
@@ -1,4 +1,4 @@
-package.describe({
+Package.describe({
   name: 'silvervue:wkwebview',
   version: '0.1.3',
   summary: 'Use the significantly better and faster WKWebView in mobile apps on iOS 8+.',
