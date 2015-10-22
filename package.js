5bf52978526d17b58b5052fcf07032ac80c16baf
Jason Thomas <jthomas@silvervue.com>
2015-10-22 13:53:12 -0600

    Point at http clone

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Cordova.depends({
   // the cordova plugin built by Meteor Core team that "emulates a server" on
   // the mobile device. Serving the files and checking for the HCP updates.
   //'com.meteor.cordova-update': 'https://github.com/practicalmeteor/com.meteor.cordova-update/tarball/a7e98f27a26b727288da81dcfd8047eccb5f6036',
-  'com.telerik.plugins.wkwebview': 'ssh://git@stash.silvervue.com/pub/wkwebview.git#b167ccdc8de56cdc71de35bb6524de3200337185'
+  'com.telerik.plugins.wkwebview': 'https://stash.silvervue.com/scm/pub/wkwebview.git#b167ccdc8de56cdc71de35bb6524de3200337185'
 });
 
 //meteor add cordova:com.meteor.cordova-update@https://github.com/practicalmeteor/com.meteor.cordova-update/tarball/a7e98f27a26b727288da81dcfd8047eccb5f6036
